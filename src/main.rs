f1da731d6418969a246856d20bc9ef61775ecc71
hcasten <hcasten@outlook.com>
2023-08-27 00:09:03 -0700

    feat(args) allow for a file to be passed in from args

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,54 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
-use calamine::{open_workbook, Xlsx, RangeDeserializerBuilder, Reader};
+use calamine::{open_workbook, RangeDeserializerBuilder, Reader, Xlsx};
+use clap::Parser;
 use native_dialog::FileDialog;
 use serde::{Deserialize, Serialize};
 
-#[serde(rename_all="PascalCase")]
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct Row {
     pub term: String,
     pub translation: String,
     pub language: String,
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[arg(short, long)]
+    path: Option<String>,
+}
+
 fn main() -> Result<()> {
     println!("Hello, world!");
-    let result = FileDialog::new()
-        .set_location("~")
-        .add_filter("text", &["xlsx"])
-        .show_open_single_file()?;
+
+    let args = CliArgs::parse();
+    let result: Option<PathBuf>;
+
+    if let Some(arg_file) = args.path {
+        result = Some(PathBuf::from(arg_file));
+    } else {
+        result = FileDialog::new()
+            .set_location("~")
+            .add_filter("text", &["xlsx"])
+            .show_open_single_file()?;
+    }
 
     if let Some(excel) = result {
         println!("{:?}", excel);
         let mut workbook: Xlsx<_> = open_workbook(excel)?;
         let range = workbook
-            .worksheet_range("Sheet 1").expect("Unable to load Sheet 1").unwrap();
+            .worksheet_range("Sheet 1")
+            .expect("Unable to load Sheet 1")
+            .unwrap();
 
-        let  iter = RangeDeserializerBuilder::new().from_range::<_, Row>(&range)?;
+        let iter = RangeDeserializerBuilder::new().from_range::<_, Row>(&range)?;
         for ele in iter {
             println!("{:?}", ele);
         }
     };
 
-    std::process::abort();
+    std::process::exit(0);
 }
